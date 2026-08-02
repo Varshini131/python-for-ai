cfec8e980bbe79cd1fe1566cb78f5e5c1fc0a6ab
Varshini <varshinimenta393@gmail.com>
2026-08-02 23:36:30 +0530

    Completed lineplot

---
+++ 04_visualization/line_plot.py
@@ -0,0 +1,19 @@
+"""
+line_plot.py
+Creating line plots
+"""
+
+import matplotlib.pyplot as plt
+
+months = ["Jan", "Feb", "Mar", "Apr", "May"]
+sales = [120, 150, 180, 170, 200]
+
+plt.plot(months, sales, marker="o")
+
+plt.title("Monthly Sales")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+
+plt.grid(True)
+
+plt.show()

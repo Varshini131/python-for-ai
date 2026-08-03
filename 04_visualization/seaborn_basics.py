06c5d41a7410db89a6b8170fbf4044c0bfe120f0
Varshini <varshinimenta393@gmail.com>
2026-08-04 00:29:09 +0530

    Completed seabron basics

---
+++ 04_visualization/seaborn_basics.py
@@ -0,0 +1,15 @@
+"""
+seaborn_basics.py
+Introduction to Seaborn
+"""
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset("tips")
+
+sns.scatterplot(data=tips, x="total_bill", y="tip")
+
+plt.title("Total Bill vs Tip")
+
+plt.show()
